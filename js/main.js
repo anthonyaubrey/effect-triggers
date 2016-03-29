8c4f47fc8f3997f41df786152b1d80bf25bd8133
anthonyaubrey <anthonyaubrey245@gmail.com>
2016-03-29 17:07:12 -0400

    Added variable and css properties to collapsing panel

--- js/main.js
+++ js/main.js
@@ -6,10 +6,17 @@ var $box = $('.box');
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
+var $btnColEx = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
 $btnShowHide.on('click', function () {
   $box.toggleClass('showbox');
 });
 
 $btnMove.on('click', function () {
   $diamond.toggleClass('btn-slide');
+});
+
+$btnColEx.on('click', function () {
+  $panel.toggleClass('collapse-panel');
 });
